66ddedfbc85e869b24c36e84c91f38c0d2136f0e
g0tie <gaultierbegue@outlook.com>
2025-05-08 04:16:42 -0400

    starting writign atoi function

---
+++ addition.asm
@@ -0,0 +1,15 @@
+%include './mylibs/strings.asm'
+%include './mylibs/numbers.asm'
+
+SECTION .data
+msg1 db '',0
+msg2 db '',0
+
+SECTION .bss
+
+
+SECTION .text
+global _start
+
+_start:
+
---
+++ mylibs/atoi.asm
@@ -0,0 +1,32 @@
+;-----------
+;int atoi(Integer number)
+;ASCII to integer 
+;-----------
+
+atoi:
+	push ebx
+	push ecx
+	push edx
+	push esi
+
+	mov esi, eax
+	mov eax, 0
+	mov ecx, 0
+
+.multiplyLoop:
+	xor ebx, ebx
+	mov bl, [esi + ecx] ;access index ecx from string array esi (memory address + offset and bl only take 1 byte of the string)
+	
+	cmp bl, 48
+	jl .finished
+	
+	cmp bl, 57
+	jg .finished
+
+
+	sub bl, 48
+	add eax, ebx
+	mov ebx, 10
+	mul ebx
+	inc ecx
+	jmp .multiplyLoop
